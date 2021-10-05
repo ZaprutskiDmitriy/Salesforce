f717bc3e51c7095a2d66b8e93ff763e067bf412c
Dmitriy Zaprutski <zaprutsciy@mail.ru>
2021-10-05 20:56:08 +0300

    Added allure reporting

--- src/main/java/tests/AccountTest.java
+++ src/main/java/tests/AccountTest.java
@@ -1,14 +1,25 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.TmsLink;
 import models.Account;
 import models.AccountFactory;
 import org.testng.annotations.Test;
+import utils.AllureUtils;
 
 import static org.testng.Assert.assertTrue;
 
 public class AccountTest extends BaseTest {
 
-    @Test
+    @Test(description = "Create account test")
+    @Description("Validation of create account functionality in Salesforce")
+    @Link("https://tut.by")
+    @Issue("ISSUE-13")
+    @TmsLink("TMS-13")
+
+
     public void createAccount() {
         boolean isOpened = loginPage
                 .open()
@@ -24,5 +35,6 @@ public class AccountTest extends BaseTest {
                 .clickNew()
                 .createAccount(account)
                 .clickSave();
+        AllureUtils.takeScreenshot(driver);
     }
 }
